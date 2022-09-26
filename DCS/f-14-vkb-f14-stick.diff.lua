0bd1f34a4efbd1c68d502e844982919d89602c19
Jianming Chen <james@ashchan.com>
2022-09-26 13:06:49 +0900

    Check in vkb f-14 grip profile for DCS F-14

---
+++ DCS/f-14-vkb-f14-stick.diff.lua
@@ -0,0 +1,135 @@
+local diff = {
+	["axisDiffs"] = {
+		["a3083cd57"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RX",
+				},
+			},
+			["name"] = "DLC / Maneuver Flaps Retract",
+		},
+	},
+	["keyDiffs"] = {
+		["d3078pnilu3078cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Store Release",
+		},
+		["d3079pnilunilcd55vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Weapon Selector Sidewinder Missiles",
+		},
+		["d3079pnilunilcd55vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Weapon Selector Off",
+		},
+		["d3079pnilunilcd55vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Weapon Selector Sparrow or Phoenix Missiles",
+		},
+		["d3079pnilunilcd55vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "Weapon Selector Gun",
+		},
+		["d3082pnilu3082cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Weapon Selector press",
+		},
+		["d3084pnilu3084cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "DLC Toggle / Countermeasure Dispense",
+		},
+		["d3085pnilu3085cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Autopilot Reference / Nosewheel Steering Toggle",
+		},
+		["d3087pnilu3087cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Trigger",
+		},
+		["d3088pnilu3088cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Trigger First Detent",
+		},
+		["dnilp2019u2019cdnilvdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Trim Pitch Down",
+		},
+		["dnilp2019u2019cdnilvdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Trim Pitch Up",
+		},
+		["dnilp2020u2020cdnilvdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Trim Roll Left Wing Down",
+		},
+		["dnilp2020u2020cdnilvdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Trim Roll Right Wing Down",
+		},
+		["dnilp2101u2101cdnilvdnilvp1vu-1"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "Wheel Brake both",
+		},
+	},
+}
+return diff
